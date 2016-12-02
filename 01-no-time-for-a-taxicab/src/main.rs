f7b9010879132b6b28b160065daf3f2407a97c05
Jake Goulding <jake.goulding@gmail.com>
2016-12-02 11:55:56 -0500

    Day 1 - Part 2

--- 01-no-time-for-a-taxicab/src/main.rs
+++ 01-no-time-for-a-taxicab/src/main.rs
@@ -1,5 +1,6 @@
 use std::str::FromStr;
 use std::error::Error;
+use std::collections::HashSet;
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 enum Turn {
@@ -64,26 +65,104 @@ impl Direction {
         }
     }
 }
+type Coordinates = (i64, i64);
 
-fn algorithm(input: &str) -> (u64, u64) {
+#[derive(Debug)]
+struct Tracker {
+    blocks_north_south: i64,
+    blocks_east_west: i64,
+    visited: HashSet<Coordinates>,
+    first_intersection: Option<Coordinates>,
+}
+
+/// Exclusive because we've already visited the starting location,
+/// inclusive so we visit the ending location.
+macro_rules! iterate_in_either_direction_exclusive_inclusive {
+    ($a:expr, $b:expr) => {
+        if $a < $b {
+            Box::new($a..($b+1)) as Box<Iterator<Item = i64>>
+        } else {
+            Box::new((($b+1)..$a).rev())
+        }.skip(1)
+    }
+}
+
+impl Tracker {
+    fn new() -> Tracker {
+        let mut visited = HashSet::default();
+        // We start at the origin
+        visited.insert((0, 0));
+
+        Tracker {
+            blocks_north_south: 0,
+            blocks_east_west: 0,
+            visited: visited,
+            first_intersection: None,
+        }
+    }
+
+    fn travel_north_south(&mut self, blocks: i64) {
+        let start = self.blocks_north_south;
+        let end = start + blocks;
+
+        let x = self.blocks_east_west;
+        for y in iterate_in_either_direction_exclusive_inclusive!(start, end) {
+            self.track_visited(x, y);
+        }
+
+        self.blocks_north_south = end;
+    }
+
+    fn travel_east_west(&mut self, blocks: i64) {
+        let start = self.blocks_east_west;
+        let end = start + blocks;
+
+        let y = self.blocks_north_south;
+        for x in iterate_in_either_direction_exclusive_inclusive!(start, end) {
+            self.track_visited(x, y);
+        }
+
+        self.blocks_east_west = end;
+    }
+
+    fn track_visited(&mut self, x: i64, y: i64) {
+        if self.first_intersection.is_some() { return }
+
+        let coord = (x, y);
+
+        if !self.visited.insert(coord) {
+            self.first_intersection = Some(coord);
+        }
+    }
+
+    fn end_position(&self) -> (u64, u64) {
+        // Only care about magnitude
+        (self.blocks_east_west.abs() as u64, self.blocks_north_south.abs() as u64)
+    }
+
+    fn first_intersection(&self) -> Option<(u64, u64)> {
+        // Only care about magnitude
+        self.first_intersection.map(|(x, y)| (x.abs() as u64, y.abs() as u64))
+    }
+}
+
+fn track_commands(input: &str) -> Tracker {
     let mut direction = Direction::North;
-    let mut blocks_north_south = 0;
-    let mut blocks_east_west = 0;
+    let mut tracker = Tracker::new();
 
     for command in input.split(",").map(|i| i.parse::<Command>().expect("Couldn't parse input")) {
         direction = direction.turn(command.turn);
         let blocks = command.blocks as i64;
 
         match direction {
-            Direction::North => blocks_north_south += blocks,
-            Direction::South => blocks_north_south -= blocks,
-            Direction::East => blocks_east_west += blocks,
-            Direction::West => blocks_east_west -= blocks,
+            Direction::North => tracker.travel_north_south(blocks),
+            Direction::South => tracker.travel_north_south(-blocks),
+            Direction::East => tracker.travel_east_west(blocks),
+            Direction::West => tracker.travel_east_west(-blocks),
         }
     }
 
-    // Only care about magnitude
-    (blocks_north_south.abs() as u64, blocks_east_west.abs() as u64)
+    tracker
 }
 
 fn main() {
@@ -97,7 +176,14 @@ fn main() {
                  R4, L4, L4, R1, R5, R3, L1, L4, L3, L4, R1, L5, L1, R2, R2, R4, R4, L5, R4, R1, \
                  L1, L1, L3, L5, L2, R4, L3, L5, L4, L1, R3";
 
-    let (blocks_north_south, blocks_east_west) = algorithm(input);
+    let tracker = track_commands(input);
+
+    let (blocks_north_south, blocks_east_west) = tracker.end_position();
+    let total = blocks_north_south + blocks_east_west;
+
+    println!("{} + {} => {}", blocks_north_south, blocks_east_west, total);
+
+    let (blocks_east_west, blocks_north_south) = tracker.first_intersection().expect("No intersection found");
     let total = blocks_north_south + blocks_east_west;
 
     println!("{} + {} => {}", blocks_north_south, blocks_east_west, total);
@@ -105,16 +191,21 @@ fn main() {
 
 #[test]
 fn example_1() {
-    assert_eq!(algorithm("R2, L3"), (3, 2));
+    assert_eq!(track_commands("R2, L3").end_position(), (2, 3));
 }
 
 #[test]
 fn example_2() {
-    assert_eq!(algorithm("R2, R2, R2"), (2, 0));
+    assert_eq!(track_commands("R2, R2, R2").end_position(), (0, 2));
 }
 
 #[test]
 fn example_3() {
-    let (ns, ew) = algorithm("R5, L5, R5, R3");
+    let (ns, ew) = track_commands("R5, L5, R5, R3").end_position();
     assert_eq!(ns + ew, 12) ;
+}
+
+#[test]
+fn example_4() {
+    assert_eq!(track_commands("R8, R4, R4, R8").first_intersection(), Some((4, 0)));
 }
