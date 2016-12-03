cdf413eadf20fbc7a6b2a572a604e64f7f335987
Jake Goulding <jake.goulding@gmail.com>
2016-12-03 10:56:41 -0500

    Day 3 - Part 2

--- 03-squares-with-three-sides/src/main.rs
+++ 03-squares-with-three-sides/src/main.rs
@@ -1,5 +1,9 @@
+extern crate itertools;
+
 use std::str::FromStr;
 use std::error::Error;
+
+use itertools::Itertools;
 
 struct MaybeTriangle {
     a: u32,
@@ -14,17 +18,14 @@ impl MaybeTriangle {
         let MaybeTriangle { a, b, c } = *self;
         a + b > c && b + c > a && a + c > b
     }
-}
 
-impl FromStr for MaybeTriangle {
-    type Err = Box<Error>;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut parts = s.split_whitespace().fuse();
-
-        let a = parts.next().ok_or("Number missing".to_string())?;
-        let b = parts.next().ok_or("Number missing".to_string())?;
-        let c = parts.next().ok_or("Number missing".to_string())?;
+    fn from_three_optional_strings(a: Option<&str>,
+                                   b: Option<&str>,
+                                   c: Option<&str>)
+                                   -> Result<Self, Box<Error>> {
+        let a = a.ok_or("Number missing".to_string())?;
+        let b = b.ok_or("Number missing".to_string())?;
+        let c = c.ok_or("Number missing".to_string())?;
 
         let a = a.parse()?;
         let b = b.parse()?;
@@ -34,22 +35,60 @@ impl FromStr for MaybeTriangle {
     }
 }
 
-fn algorithm(input: &str) -> usize {
+impl FromStr for MaybeTriangle {
+    type Err = Box<Error>;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut parts = s.split_whitespace().fuse();
+        MaybeTriangle::from_three_optional_strings(parts.next(), parts.next(), parts.next())
+    }
+}
+
+fn horizontal_algorithm(input: &str) -> usize {
     input.lines()
         .filter_map(|l| l.parse::<MaybeTriangle>().ok())
         .filter(MaybeTriangle::is_valid)
         .count()
 }
 
+fn vertical_algorithm(input: &str) -> usize {
+    input.lines().tuples().map(|(line1, line2, line3)| {
+        let mut line1 = line1.split_whitespace().fuse();
+        let mut line2 = line2.split_whitespace().fuse();
+        let mut line3 = line3.split_whitespace().fuse();
+
+        (0..3).filter_map(|_| {
+            let a = line1.next();
+            let b = line2.next();
+            let c = line3.next();
+
+            MaybeTriangle::from_three_optional_strings(a, b, c).ok()
+        }).filter(MaybeTriangle::is_valid).count()
+    }).sum()
+}
+
 fn main() {
     let input = include_str!("input.txt");
 
-    let count = algorithm(input);
+    let count = horizontal_algorithm(input);
+    println!("There were {} valid triangles", count);
 
+    let count = vertical_algorithm(input);
     println!("There were {} valid triangles", count);
 }
 
 #[test]
 fn example_1() {
-    assert_eq!(0, algorithm("5 10 25"));
+    assert_eq!(0, horizontal_algorithm("5 10 25"));
+}
+
+#[test]
+fn example_2() {
+    let input = "101 301 501
+                 102 302 502
+                 103 303 503
+                 201 401 601
+                 202 402 602
+                 203 403 603";
+    assert_eq!(6, vertical_algorithm(input));
 }
