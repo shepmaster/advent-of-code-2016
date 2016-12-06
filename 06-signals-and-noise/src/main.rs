56927286e58412c042f1053f250c784c392f2895
Jake Goulding <jake.goulding@gmail.com>
2016-12-06 08:59:39 -0500

    Day 6 - Part 2

--- 06-signals-and-noise/src/main.rs
+++ 06-signals-and-noise/src/main.rs
@@ -1,6 +1,11 @@
 use std::collections::BTreeMap;
+use std::collections::btree_map;
 
-fn decode_message(input: &str) -> String {
+type CharacterFrequency = (char, usize);
+
+fn decode_message<F>(input: &str, ordering: F) -> String
+    where F: Fn(btree_map::IntoIter<char, usize>) -> Option<CharacterFrequency>,
+{
     let mut by_position = BTreeMap::new();
 
     for line in input.lines() {
@@ -12,18 +17,40 @@ fn decode_message(input: &str) -> String {
 
     by_position.into_iter()
         .flat_map(|(_, by_letter)| {
-            by_letter.into_iter()
-                .max_by_key(|&(_, cnt)| cnt)
+            ordering(by_letter.into_iter())
                 .map(|(chr, _)| chr)
         })
         .collect()
 }
 
-fn main() {
-    println!("The message is {}", decode_message(include_str!("input.txt")));
+fn decode_message_max(input: &str) -> String {
+    decode_message(input, |i| Iterator::max_by_key(i, |&(_, i)| i))
 }
 
-#[test]
-fn example_1() {
-    assert_eq!(decode_message(include_str!("test-input.txt")), "easter");
+fn decode_message_min(input: &str) -> String {
+    decode_message(input, |i| Iterator::min_by_key(i, |&(_, i)| i))
+}
+
+fn main() {
+    let input = include_str!("input.txt");
+
+    println!("The message is {}", decode_message_max(input));
+    println!("The message is really {}", decode_message_min(input));
+}
+
+#[cfg(test)]
+mod test {
+    use super::{decode_message_max, decode_message_min};
+
+    const TEST_INPUT: &'static str = include_str!("test-input.txt");
+
+    #[test]
+    fn example_1() {
+        assert_eq!(decode_message_max(TEST_INPUT), "easter");
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(decode_message_min(TEST_INPUT), "advent");
+    }
 }
